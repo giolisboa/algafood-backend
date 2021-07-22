aa1dce69f50234c46de7b7b7d41dc061fc153802
Giovani Lisboa <giovanirodrigueslisboa@gmail.com>
2021-07-22 19:16:36 -0300

    Aula 2.12

---
+++ src/main/java/com/algaworks/algafood/MeuPrimeiroController.java
@@ -0,0 +1,28 @@
+package com.algaworks.algafood;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.algaworks.algafood.di.modelo.Cliente;
+import com.algaworks.algafood.di.service.AtivacaoClienteService;
+
+@Controller
+public class MeuPrimeiroController {
+	
+    private AtivacaoClienteService ativacaoClienteService;
+    
+	public MeuPrimeiroController(AtivacaoClienteService ativacaoClienteService) {
+        this.ativacaoClienteService = ativacaoClienteService;
+    }
+
+    @GetMapping("/hello")
+	@ResponseBody
+	public String hello() {
+        Cliente c = new Cliente("Giovani", "giovani@gmail.com", "8888888");
+        
+        ativacaoClienteService.ativar(c);
+        
+		return "Giovani Lisboa";
+	}
+}
