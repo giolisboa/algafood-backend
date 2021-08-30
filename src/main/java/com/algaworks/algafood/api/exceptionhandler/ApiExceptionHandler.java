865f4e99f878be690ce9140d796fcd412c58e491
Giovani Lisboa <giovanirodrigueslisboa@gmail.com>
2021-08-30 20:27:35 -0300

    8.13. Tratando exceções globais com @ExceptionHandler e @ControllerAdvice

---
+++ src/main/java/com/algaworks/algafood/api/exceptionhandler/ApiExceptionHandler.java
@@ -0,0 +1,47 @@
+package com.algaworks.algafood.api.exceptionhandler;
+
+import java.time.LocalDateTime;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpMediaTypeException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.algaworks.algafood.domain.exception.EntidadeNaoEncontradaException;
+import com.algaworks.algafood.domain.exception.NegocioException;
+
+@ControllerAdvice
+public class ApiExceptionHandler {
+
+    @ExceptionHandler(EntidadeNaoEncontradaException.class)
+    public ResponseEntity<?> tratarEntidadeNaoEncontradaException(EntidadeNaoEncontradaException e) {
+        Problema problema = new Problema();
+
+        problema.setDataHora(LocalDateTime.now());
+        problema.setMensagem(e.getMessage());
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(problema);
+    }
+
+    @ExceptionHandler(NegocioException.class)
+    public ResponseEntity<?> tratarNegocioException(NegocioException e) {
+        Problema problema = new Problema();
+
+        problema.setDataHora(LocalDateTime.now());
+        problema.setMensagem(e.getMessage());
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(problema);
+    }
+
+    @ExceptionHandler(HttpMediaTypeException.class)
+    public ResponseEntity<?> tratarNegocioException() {
+        Problema problema = new Problema();
+
+        problema.setDataHora(LocalDateTime.now());
+        problema.setMensagem("O tipo de mídia não é aceito.");
+
+        return ResponseEntity.status(HttpStatus.UNSUPPORTED_MEDIA_TYPE).body(problema);
+    }
+
+}
