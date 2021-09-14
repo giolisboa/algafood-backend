a676507aaa22f217b76e063a1cae4204fee9d4f7
Giovani Lisboa <giovanirodrigueslisboa@gmail.com>
2021-09-13 21:54:32 -0300

    Removido o PATCH do RestauranteController e feito outros ajustes

--- src/main/java/com/algaworks/algafood/domain/model/Cidade.java
+++ src/main/java/com/algaworks/algafood/domain/model/Cidade.java
@@ -15,7 +15,7 @@ import javax.validation.constraints.NotNull;
 import javax.validation.groups.ConvertGroup;
 import javax.validation.groups.Default;
 
-import com.algaworks.algafood.Groups;
+import com.algaworks.algafood.core.validation.Groups;
 
 @Entity
 public class Cidade {
