0321b082e7ec84c2bdf3caf8ae573791144d14ab
Giovani Lisboa <giovanirodrigueslisboa@gmail.com>
2021-09-16 19:03:14 -0300

    11.1. Analisando e definindo melhor o escopo das transações

--- src/main/java/com/algaworks/algafood/domain/service/RestauranteService.java
+++ src/main/java/com/algaworks/algafood/domain/service/RestauranteService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.algaworks.algafood.domain.exception.EntidadeEmUsoException;
 import com.algaworks.algafood.domain.exception.RestauranteNaoEncontradoException;
@@ -22,6 +23,7 @@ public class RestauranteService {
     @Autowired
     private CozinhaService cozinhaService;
 
+    @Transactional
     public Restaurante salvar(Restaurante restaurante) {
         Long idCozinha = restaurante.getCozinha().getId();
 
@@ -32,6 +34,7 @@ public class RestauranteService {
         return restauranteRepository.save(restaurante);
     }
 
+    @Transactional
     public void excluir(Long idRestaurante) {
         try {
             restauranteRepository.deleteById(idRestaurante);
