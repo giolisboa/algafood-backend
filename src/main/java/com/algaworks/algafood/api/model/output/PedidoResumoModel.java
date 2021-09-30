72fecf49c2e5761727afbabc9f5645c6fc108459
Giovani Lisboa <giovanirodrigueslisboa@gmail.com>
2021-09-30 20:11:02 -0300

    12.20. Otimizando a query de pedidos e retornando model resumido na listagem

---
+++ src/main/java/com/algaworks/algafood/api/model/output/PedidoResumoModel.java
@@ -0,0 +1,81 @@
+package com.algaworks.algafood.api.model.output;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+
+public class PedidoResumoModel {
+
+    private Long id;
+    private BigDecimal subtotal;
+    private BigDecimal taxaFrete;
+    private BigDecimal valorTotal;
+    private String status;
+    private OffsetDateTime dataCriacao;
+    private RestauranteResumoModel restaurante;
+    private UsuarioModel cliente;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public BigDecimal getSubtotal() {
+        return subtotal;
+    }
+
+    public void setSubtotal(BigDecimal subtotal) {
+        this.subtotal = subtotal;
+    }
+
+    public BigDecimal getTaxaFrete() {
+        return taxaFrete;
+    }
+
+    public void setTaxaFrete(BigDecimal taxaFrete) {
+        this.taxaFrete = taxaFrete;
+    }
+
+    public BigDecimal getValorTotal() {
+        return valorTotal;
+    }
+
+    public void setValorTotal(BigDecimal valorTotal) {
+        this.valorTotal = valorTotal;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public OffsetDateTime getDataCriacao() {
+        return dataCriacao;
+    }
+
+    public void setDataCriacao(OffsetDateTime dataCriacao) {
+        this.dataCriacao = dataCriacao;
+    }
+
+    public RestauranteResumoModel getRestaurante() {
+        return restaurante;
+    }
+
+    public void setRestaurante(RestauranteResumoModel restaurante) {
+        this.restaurante = restaurante;
+    }
+
+    public UsuarioModel getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(UsuarioModel cliente) {
+        this.cliente = cliente;
+    }
+
+}
