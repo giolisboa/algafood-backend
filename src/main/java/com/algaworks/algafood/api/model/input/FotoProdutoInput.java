14b3070e9fdbe3c41376af863fd2685b0b64a8f4
Giovani Lisboa <giovanirodrigueslisboa@gmail.com>
2021-10-16 10:57:40 -0300

    14.3. Validando o tamanho máximo do arquivo

--- src/main/java/com/algaworks/algafood/api/model/input/FotoProdutoInput.java
+++ src/main/java/com/algaworks/algafood/api/model/input/FotoProdutoInput.java
@@ -1,10 +1,19 @@
 package com.algaworks.algafood.api.model.input;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 import org.springframework.web.multipart.MultipartFile;
+
+import com.algaworks.algafood.core.validation.FileSize;
 
 public class FotoProdutoInput {
 
+    @NotNull
+    @FileSize(max = "500KB")
     private MultipartFile arquivo;
+
+    @NotBlank
     private String descricao;
 
     public MultipartFile getArquivo() {
