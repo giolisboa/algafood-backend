aa1de64d37d10e5f6df5a0f92aede2de7fa97728
Giovani Lisboa <giovanirodrigueslisboa@gmail.com>
2021-10-17 17:56:07 -0300

    14.6. Implementando serviço de cadastro de foto de produto

--- src/main/java/com/algaworks/algafood/api/controller/RestauranteProdutoFotoController.java
+++ src/main/java/com/algaworks/algafood/api/controller/RestauranteProdutoFotoController.java
@@ -1,38 +1,54 @@
 package com.algaworks.algafood.api.controller;
 
-import java.nio.file.Path;
-import java.util.UUID;
-
 import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
+import com.algaworks.algafood.api.assembler.FotoProdutoModelAssembler;
 import com.algaworks.algafood.api.model.input.FotoProdutoInput;
+import com.algaworks.algafood.api.model.output.FotoProdutoModel;
+import com.algaworks.algafood.domain.model.FotoProduto;
+import com.algaworks.algafood.domain.model.Produto;
+import com.algaworks.algafood.domain.service.FotoProdutoService;
+import com.algaworks.algafood.domain.service.ProdutoService;
 
 @RestController
 @RequestMapping(value = "/restaurantes/{idRestaurante}/produtos/{idProduto}/foto")
 public class RestauranteProdutoFotoController {
 
+    @Autowired
+    private FotoProdutoService fotoProdutoService;
+
+    @Autowired
+    private ProdutoService produtoService;
+
+    @Autowired
+    private FotoProdutoModelAssembler fotoProdutoModelAssembler;
+
     @PutMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public void atualizarFoto(@PathVariable Long idRestaurante, @PathVariable Long idProduto,
+    public FotoProdutoModel atualizarFoto(@PathVariable Long idRestaurante, @PathVariable Long idProduto,
             @Valid FotoProdutoInput fotoProdutoInput) {
 
-        var nomeArquivo = UUID.randomUUID().toString()
-                .concat("-")
-                .concat(fotoProdutoInput.getArquivo().getOriginalFilename());
+        Produto produto = produtoService.buscar(idRestaurante, idProduto);
 
-        var arquivoFoto = Path.of("C:/Users/Usuario/Documents/Temporário/CatalogoCursoSpring", nomeArquivo);
+        MultipartFile arquivo = fotoProdutoInput.getArquivo();
 
-        try {
-            fotoProdutoInput.getArquivo().transferTo(arquivoFoto);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
+        FotoProduto foto = new FotoProduto();
+        foto.setProduto(produto);
+        foto.setDescricao(fotoProdutoInput.getDescricao());
+        foto.setContentType(arquivo.getContentType());
+        foto.setTamanho(arquivo.getSize());
+        foto.setNomeArquivo(arquivo.getOriginalFilename());
 
+        FotoProduto fotoSalva = fotoProdutoService.salvar(foto);
+
+        return fotoProdutoModelAssembler.toModel(fotoSalva);
     }
 
 }
---
+++ src/main/java/com/algaworks/algafood/infrastructure/repository/ProdutoRepositoryImpl.java
@@ -0,0 +1,22 @@
+package com.algaworks.algafood.infrastructure.repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.springframework.transaction.annotation.Transactional;
+
+import com.algaworks.algafood.domain.model.FotoProduto;
+import com.algaworks.algafood.domain.repository.ProdutoRepositoryQueries;
+
+public class ProdutoRepositoryImpl implements ProdutoRepositoryQueries {
+
+    @PersistenceContext
+    private EntityManager manager;
+
+    @Transactional
+    @Override
+    public FotoProduto save(FotoProduto foto) {
+        return manager.merge(foto);
+    }
+
+}
