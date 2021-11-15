31579faed033e8c79ddb7139146b0459e79f2120
Giovani Lisboa <giovanirodrigueslisboa@gmail.com>
2021-11-15 20:44:37 -0300

    18.31. Desafio: descrevendo documentação de endpoints de fluxo de pedidos

--- src/main/java/com/algaworks/algafood/api/openapi/controller/PedidoControllerOpenApi.java
+++ src/main/java/com/algaworks/algafood/api/openapi/controller/PedidoControllerOpenApi.java
@@ -35,4 +35,28 @@ public interface PedidoControllerOpenApi {
     PedidoModel buscar(
             @ApiParam(value = "Código de um pedido", example = "f9981ca4-5a5e-4da3-af04-933861df3e55", required = true) String codigoPedido);
 
+    @ApiOperation("Confirmação de pedido")
+    @ApiResponses({
+            @ApiResponse(code = 204, message = "Pedido confirmado com sucesso"),
+            @ApiResponse(code = 404, message = "Pedido não encontrado", response = Problem.class)
+    })
+    void confirmar(
+            @ApiParam(value = "Código do pedido", example = "f9981ca4-5a5e-4da3-af04-933861df3e55", required = true) String codigoPedido);
+
+    @ApiOperation("Cancelamento de pedido")
+    @ApiResponses({
+            @ApiResponse(code = 204, message = "Pedido cancelado com sucesso"),
+            @ApiResponse(code = 404, message = "Pedido não encontrado", response = Problem.class)
+    })
+    void cancelar(
+            @ApiParam(value = "Código do pedido", example = "f9981ca4-5a5e-4da3-af04-933861df3e55", required = true) String codigoPedido);
+
+    @ApiOperation("Registrar entrega de pedido")
+    @ApiResponses({
+            @ApiResponse(code = 204, message = "Entrega de pedido registrada com sucesso"),
+            @ApiResponse(code = 404, message = "Pedido não encontrado", response = Problem.class)
+    })
+    void entregar(
+            @ApiParam(value = "Código do pedido", example = "f9981ca4-5a5e-4da3-af04-933861df3e55", required = true) String codigoPedido);
+
 }
