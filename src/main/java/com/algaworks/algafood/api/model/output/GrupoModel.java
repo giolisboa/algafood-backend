a539bb94b547d30f3527da5e94f04f3a0627f7b8
Giovani Lisboa <giovanirodrigueslisboa@gmail.com>
2021-11-15 16:34:45 -0300

    18.18. Desafio: descrevendo documentação de endpoints de grupos

--- src/main/java/com/algaworks/algafood/api/model/output/GrupoModel.java
+++ src/main/java/com/algaworks/algafood/api/model/output/GrupoModel.java
@@ -1,8 +1,13 @@
 package com.algaworks.algafood.api.model.output;
 
+import io.swagger.annotations.ApiModelProperty;
+
 public class GrupoModel {
 
+    @ApiModelProperty(example = "1")
     private Long id;
+
+    @ApiModelProperty(example = "Gerente")
     private String nome;
 
     public Long getId() {
