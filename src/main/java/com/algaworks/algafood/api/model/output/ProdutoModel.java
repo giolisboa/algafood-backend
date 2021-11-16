d99a4ab89e254ccbe73f5918b08fb02949eaeddd
Giovani Lisboa <giovanirodrigueslisboa@gmail.com>
2021-11-15 21:01:00 -0300

    18.34. Desafio: descrevendo documentação de endpoints de produtos

--- src/main/java/com/algaworks/algafood/api/model/output/ProdutoModel.java
+++ src/main/java/com/algaworks/algafood/api/model/output/ProdutoModel.java
@@ -2,12 +2,23 @@ package com.algaworks.algafood.api.model.output;
 
 import java.math.BigDecimal;
 
+import io.swagger.annotations.ApiModelProperty;
+
 public class ProdutoModel {
 
+    @ApiModelProperty(example = "1")
     private Long id;
+
+    @ApiModelProperty(example = "Espetinho de Cupim")
     private String nome;
+
+    @ApiModelProperty(example = "Acompanha farinha, mandioca e vinagrete")
     private String descricao;
+
+    @ApiModelProperty(example = "12.50")
     private BigDecimal preco;
+
+    @ApiModelProperty(example = "true")
     private Boolean ativo;
 
     public Long getId() {
