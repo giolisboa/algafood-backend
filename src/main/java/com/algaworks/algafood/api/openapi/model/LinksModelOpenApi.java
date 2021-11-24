29cd7cf729f59a0d8446fb7e9de7ae8964f37cca
Giovani Lisboa <giovanirodrigueslisboa@gmail.com>
2021-11-23 23:07:00 -0300

    19.39. Corrigindo as propriedades de links na documentação

---
+++ src/main/java/com/algaworks/algafood/api/openapi/model/LinksModelOpenApi.java
@@ -0,0 +1,42 @@
+package com.algaworks.algafood.api.openapi.model;
+
+import io.swagger.annotations.ApiModel;
+
+@ApiModel("Links")
+public class LinksModelOpenApi {
+
+    private LinkModel rel;
+
+    public LinkModel getRel() {
+        return rel;
+    }
+
+    public void setRel(LinkModel rel) {
+        this.rel = rel;
+    }
+
+    @ApiModel("Link")
+    private class LinkModel {
+
+        private String href;
+        private boolean templated;
+
+        public String getHref() {
+            return href;
+        }
+
+        public void setHref(String href) {
+            this.href = href;
+        }
+
+        public boolean isTemplated() {
+            return templated;
+        }
+
+        public void setTemplated(boolean templated) {
+            this.templated = templated;
+        }
+
+    }
+
+}
--- src/main/java/com/algaworks/algafood/core/springfox/SpringFoxConfig.java
+++ src/main/java/com/algaworks/algafood/core/springfox/SpringFoxConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.hateoas.Links;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.context.request.ServletWebRequest;
@@ -18,6 +19,7 @@ import com.algaworks.algafood.api.exceptionhandler.Problem;
 import com.algaworks.algafood.api.model.output.CozinhaModel;
 import com.algaworks.algafood.api.model.output.PedidoResumoModel;
 import com.algaworks.algafood.api.openapi.model.CozinhasModelOpenApi;
+import com.algaworks.algafood.api.openapi.model.LinksModelOpenApi;
 import com.algaworks.algafood.api.openapi.model.PageableModelOpenApi;
 import com.algaworks.algafood.api.openapi.model.PedidosResumoModelOpenApi;
 import com.fasterxml.classmate.TypeResolver;
@@ -59,6 +61,7 @@ public class SpringFoxConfig implements WebMvcConfigurer {
                 .additionalModels(typeResolver.resolve(Problem.class))
                 .ignoredParameterTypes(ServletWebRequest.class)
                 .directModelSubstitute(Pageable.class, PageableModelOpenApi.class)
+                .directModelSubstitute(Links.class, LinksModelOpenApi.class)
                 .alternateTypeRules(AlternateTypeRules.newRule(typeResolver.resolve(Page.class, CozinhaModel.class),
                         CozinhasModelOpenApi.class))
                 .alternateTypeRules(AlternateTypeRules.newRule(
