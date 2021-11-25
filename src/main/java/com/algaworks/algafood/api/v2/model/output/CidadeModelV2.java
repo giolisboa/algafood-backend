a0ba4f4e68e366805cb5fd970581a53e318168e9
Giovani Lisboa <giovanirodrigueslisboa@gmail.com>
2021-11-24 21:34:04 -0300

    20.11. Implementando o versionamento da API por Media Type

---
+++ src/main/java/com/algaworks/algafood/api/v2/model/output/CidadeModelV2.java
@@ -0,0 +1,52 @@
+package com.algaworks.algafood.api.v2.model.output;
+
+import org.springframework.hateoas.RepresentationModel;
+import org.springframework.hateoas.server.core.Relation;
+
+import io.swagger.annotations.ApiModelProperty;
+
+@Relation(collectionRelation = "cidades")
+public class CidadeModelV2 extends RepresentationModel<CidadeModelV2> {
+
+    @ApiModelProperty(example = "1")
+    private Long idCidade;
+
+    @ApiModelProperty(example = "Uberlândia")
+    private String nomeCidade;
+
+    private Long idEstado;
+    private String nomeEstado;
+
+    public Long getIdCidade() {
+        return idCidade;
+    }
+
+    public void setIdCidade(Long idCidade) {
+        this.idCidade = idCidade;
+    }
+
+    public String getNomeCidade() {
+        return nomeCidade;
+    }
+
+    public void setNomeCidade(String nomeCidade) {
+        this.nomeCidade = nomeCidade;
+    }
+
+    public Long getIdEstado() {
+        return idEstado;
+    }
+
+    public void setIdEstado(Long idEstado) {
+        this.idEstado = idEstado;
+    }
+
+    public String getNomeEstado() {
+        return nomeEstado;
+    }
+
+    public void setNomeEstado(String nomeEstado) {
+        this.nomeEstado = nomeEstado;
+    }
+
+}
