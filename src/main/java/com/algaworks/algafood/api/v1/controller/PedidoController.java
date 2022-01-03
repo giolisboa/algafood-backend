0e980735e2c7b35b6df81f0a597476cb57cc5dda
Giovani Lisboa <giovanirodrigueslisboa@gmail.com>
2022-01-03 15:03:03 -0300

    23.29. Restringindo acessos com @PostAuthorize

--- src/main/java/com/algaworks/algafood/api/v1/controller/PedidoController.java
+++ src/main/java/com/algaworks/algafood/api/v1/controller/PedidoController.java
@@ -32,6 +32,7 @@ import com.algaworks.algafood.api.v1.openapi.controller.PedidoControllerOpenApi;
 import com.algaworks.algafood.core.data.PageWrapper;
 import com.algaworks.algafood.core.data.PageableTranslator;
 import com.algaworks.algafood.core.security.AlgaSecurity;
+import com.algaworks.algafood.core.security.CheckSecurity;
 import com.algaworks.algafood.domain.exception.FormaPagamentoNaoEncontradaException;
 import com.algaworks.algafood.domain.exception.NegocioException;
 import com.algaworks.algafood.domain.exception.RestauranteNaoEncontradoException;
@@ -79,6 +80,7 @@ public class PedidoController implements PedidoControllerOpenApi {
         return pagedResourcesAssembler.toModel(pedidosPage, pedidoResumoModelAssembler);
     }
 
+    @CheckSecurity.Pedidos.PodeBuscar
     @Override
     @GetMapping("/{codigoPedido}")
     public PedidoModel buscar(@PathVariable String codigoPedido) {
